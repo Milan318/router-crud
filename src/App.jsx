41baa89efac257f5b172f56d6fb1c9e9494aaf98
Milan318 <milanmandaviya7495@gmail.com>
2025-05-23 16:58:59 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Employee from './components/Employee'
+
+function App() {
+    const [employee, setEmployee] = useState({});
+    const [empList, setEmpList] = useState([]);
+
+    const handleChange=()=>{
+      const{name,value}=empList.target;
+      setEmployee({...employee,[name]:value});
+    }
+
+    
+  return (
+    <>
+    <Header/>
+    <Employee
+     employee={employee}
+     empList={empList}
+     handleChang={handleChange}
+    />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Employee.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+function Employee(props) {
+    let {handleChange,employee}=props
+  return (
+    <>
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-md-6 mx-auto">
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                name="ename"
+                value={employee.ename || ''}
+                onChange={handleChange}
+                id="ename"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">
+                Post
+              </label>
+              <input
+                type="text"
+                  value={employee.post || ''}
+                onChange={handleChange}
+                name="post"
+                id="post"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">
+                Salary
+              </label>
+              <input
+                type="text"
+                name="salary"
+                id="salary"
+                className="form-control"
+              />
+            </div>
+            <button className="btn btn-dark">Add Data</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Employee;
